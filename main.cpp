f4608f2b52bb28a223dc1775a2d87fc26865f4c2
dongliang <dongliang@sygole.com>
2024-12-26 15:24:28 +0800

    spiralOrder

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 int main()
 {
 	Solution test;
-	vector<int> input = { 1,2,3,4,5,6,7 };
-	test.rotate(input, 3);
-	for (const auto &mem : input) {
+	//vector<vector<int>> input = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
+	vector<vector<int>> input = {{3},{2}};
+	vector<int> res;
+	res = test.spiralOrder(input);
+	for (const auto &mem : res) {
 		cout << mem << ',';
 	}
 	cout << endl;
